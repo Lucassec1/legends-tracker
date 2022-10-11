15a9fd41c7cd1f020ee424b814d0dd860fca1fbf
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-10-11 09:16:15 -0300

    chore: creating folders for global styles :wrench:

---
+++ src/app/lib/lib.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonsComponent } from './buttons/buttons.component';
+
+
+
+@NgModule({
+  declarations: [
+    ButtonsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class LibModule { }
