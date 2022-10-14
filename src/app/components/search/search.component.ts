aa945085a49c7cb90c344b2099ef22f1b3fb733d
dayannebugarim <dayannebugarim1@gmail.com>
2022-10-14 10:34:52 -0300

    :sparkles: feat: base search component

--- src/app/components/search/search.component.ts
+++ src/app/components/search/search.component.ts
@@ -1,7 +1,12 @@
+import { SearchTypeOptions } from './../core/types/search';
 import { Component, OnInit, Input } from '@angular/core';
 
+type size = 'md' | 'lg';
+type filter = 'true' | 'false';
+
 export interface SearchProps {
-  filter?: boolean;
+  filter?: string;
+  size?: string;
   placeholder?: string;
 }
 
@@ -11,12 +16,14 @@ export interface SearchProps {
   styleUrls: ['./search.component.scss']
 })
 export class SearchComponent implements OnInit {
-  @Input() filter? = false;
-  @Input() placeholder = 'Procure um usuário...';
+  @Input() filter:filter = 'false';
+  @Input() size?:size = 'md';
+  @Input() placeholder? = 'Procure por um usuário...';
 
-  constructor() { }
+  public type!: SearchTypeOptions;
 
   ngOnInit(): void {
+    this.type = SearchTypeOptions[this.filter]
   }
 
 }
