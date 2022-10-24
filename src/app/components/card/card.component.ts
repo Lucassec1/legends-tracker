e12bc7fbe8f4521fcb1f76eb6bab0fcd981c2e24
dayannebugarim <dayannebugarim1@gmail.com>
2022-10-24 12:04:18 -0300

    :sparkles: feat: dinamic match card(#12)

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { championSquareImgURL, summonerSpellImgURL } from './../../../services/api';
+import { MatchResultOptions, SummonerSpellOptions } from './../core/types/card';
+import { Component, OnInit, Input } from '@angular/core';
+
+type Type = 'match-history';
+type Victory = 'true' | 'false' | 'undefined';
+type SummonerSpell = 'barrier' | 'cleanse' | 'ignite' | 'exhaust' | 'flash' | 'ghost' | 'heal' | 'clarity' | 'smite' | 'teleport';
 
 @Component({
   selector: 'app-card',
@@ -6,10 +12,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./card.component.scss']
 })
 export class CardComponent implements OnInit {
+  @Input() type: Type = 'match-history';
 
-  constructor() { }
+  //Math History Type Inputs
+  @Input() victory: Victory = 'undefined';
+  @Input() champion: String = 'lee sin';
+  @Input() firstSummonerSpell: SummonerSpell = 'ignite'; 
+  @Input() secondSummonerSpell: SummonerSpell = 'flash'; 
+  @Input() kill: Number = 0;
+  @Input() death: Number = 0;
+  @Input() assistance: Number = 0;
+
+  public matchResult?: MatchResultOptions;
+  public firstSummonerSpellName?: SummonerSpellOptions;
+  public secondSummonerSpellName?: SummonerSpellOptions;
+  public summonerSpellImgURL = summonerSpellImgURL;
+  public championSquareImgURL = championSquareImgURL;
+
+  constructor() {  }
+
+  championName() {
+    const championNameArr: string[] = this.champion.split(' ');
+    let formatedChampionName: string = '';
+
+    for (let i = 0; i < championNameArr.length; i++) {
+      formatedChampionName = formatedChampionName + (championNameArr[i].charAt(0).toUpperCase() + championNameArr[i].slice(1));
+    }
+    console.log(formatedChampionName)
+    return formatedChampionName;
+  }
+
+  kda() {
+    const kda = ( Number(this.kill) + Number(this.assistance) ) / Number(this.death)
+    return kda.toFixed(1);
+  }
 
   ngOnInit(): void {
+    this.kda()
+    this.matchResult = MatchResultOptions[this.victory];
+    this.firstSummonerSpellName = SummonerSpellOptions[this.firstSummonerSpell];
+    this.secondSummonerSpellName = SummonerSpellOptions[this.secondSummonerSpell];
   }
 
 }
