161f1babeb62a5261b65e374c32b6eeba618ac03
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-10-31 22:15:25 -0300

    :test_tube: test: adding test from tab group(#10)

--- src/app/components/tab-group/tab-group.component.spec.ts
+++ src/app/components/tab-group/tab-group.component.spec.ts
@@ -7,11 +7,11 @@ const selectEvent = jest.fn();
 
 const mockTabs = [
   {
-    label: 'Tab 1',
+    label: 'First Tab',
     selected: false,
   },
   {
-    label: 'Tab 2',
+    label: 'Second Tab',
     selected: false,
   },
 ];
@@ -30,15 +30,94 @@ const sut = async (customProps: AppTabGroupProps = {
   return { element: screen.getByTestId('app-tab-group'), event: selectEvent };
 };
 
+const sizes: Array<AppTabGroupProps['size']> = [
+  'sm',
+  'md',
+  'lg'
+];
+
 describe('TabGroupComponent', () => {
   it('should render tabs in horizontal by default', async () => {
-    const rendered = await sut()
-    expect(rendered.element).not.toHaveClass('app-tab-group-column');
+    const tabGroup = await sut();
+    expect(tabGroup.element).not.toHaveClass('app-tab-group-column');
     expect(screen.getByText(mockTabs[0].label)).toHaveClass('app-tab-border-bottom');
   });
 
   it('should render tabs with border bottom by default', async () => {
     await sut();
     expect(screen.getByText(mockTabs[0].label)).toHaveClass('app-tab-border-bottom');
+  });
+
+  it('should render tabs component in vertical', async () => {
+    const tabGroup = await sut({
+      direction: 'vertical',
+      tabs: mockTabs,
+      selected: {
+        emit: selectEvent
+      } as SafeAny,
+    });
+    expect(tabGroup.element.childNodes[0]).toHaveClass('app-tab-group-column');
+  })
+
+  it('should emit tab selected when clicked', async () => {
+    const tabGroup = await sut();
+    fireEvent.click(screen.getByText(mockTabs[0].label));
+    expect(tabGroup.event).toHaveBeenCalledWith({
+      label: mockTabs[0].label,
+      selected: true
+    });
+  });
+
+  it('should has border right when direction is vertical', async () => {
+    await sut({
+      direction: 'vertical',
+      tabs: mockTabs,
+      selected: {
+        emit: selectEvent
+      } as SafeAny,
+    });
+    expect(screen.getByText(mockTabs[0].label)).toHaveClass('app-tab-border-right');
+  });
+
+  it('should render border in left when left is informed', async () => {
+    await sut({
+      direction: 'vertical',
+      border: 'left',
+      tabs: mockTabs,
+      selected: {
+        emit: selectEvent
+      } as SafeAny,
+    });
+    expect(screen.getByText(mockTabs[0].label)).toHaveClass('app-tab-border-left');
+  });
+
+  it.each(sizes)('should render tabs with %s size', async (size) => {
+    await sut({
+      direction: 'vertical',
+      tabs: mockTabs,
+      size: size as TabSize,
+      selected: {
+        emit: selectEvent
+      } as SafeAny,
+    });
+    expect(screen.getByText(mockTabs[0].label)).toHaveClass(`app-tab-${size}`);
+  });
+
+  it('should emit selected tab when double clicked', async () => {
+    const tabGroup = await sut();
+    fireEvent.click(screen.getByText(mockTabs[0].label));
+    fireEvent.click(screen.getByText(mockTabs[1].label));
+    expect(tabGroup.event).toHaveBeenCalledWith({
+      label: mockTabs[1].label,
+      selected: true
+    });
+  });
+
+  it('should validate if the event was issued twice', async () => {
+    selectEvent.mockClear();
+    const tabs = await sut();
+    fireEvent.click(screen.getByText(mockTabs[1].label));
+    fireEvent.click(screen.getByText(mockTabs[1].label));
+    expect(tabs.event).toHaveBeenCalledTimes(2);
   });
 });
