974e51efc89425f9e99b23afa11f8ed2fc6572aa
Lucassec1 <lucasemmanuelcs@gmail.com>
2022-11-07 10:00:18 -0300

    :sparkles: feat: updated icon component(#25)

--- src/app/components/icon/icon.component.spec.ts
+++ src/app/components/icon/icon.component.spec.ts
@@ -7,7 +7,6 @@ const sut = async (
   await render(AppIconComponent, {
     componentProperties: customProps,
   });
-  // return document.getElementById('app-icon-' + customProps.type);
   return screen.getByTestId('iconElement');
 };
 
@@ -18,32 +17,35 @@ describe('IconComponent', () => {
     expect(icon).toBeTruthy();
   });
 
-  // it('should render correct size', async () => {
-  //   const icon = await sut();
-  //   const defaultSize = "17px"
-  //   expect(icon.getElementsByTagName('width')).toBe(defaultSize);
-  //   expect(icon.getElementsByTagName('height')).toBe(defaultSize);
-  // });
+  it('should render correct size', async () => {
+    const icon = await sut();
+    const defaultSize = "17px"
+    expect(icon.style.width).toBe(defaultSize);
+    expect(icon.style.height).toBe(defaultSize);
+  });
 
-  // it('should render icon with default color', async () => {
-  //   const defaultColor = '#fcfaff';
-  //   expect((await sut()).getElementsByClassName('stroke')).toBe(defaultColor);
-  // });
+  it('should render icon with default color', async () => {
+    const icon = await sut();
+    const defaultColor = '#fcfaff';
+    expect(icon.style.stroke).toBe(defaultColor);
+  });
 
-  // it('should render icon with custom size', async () => {
-  //   const size = 60;
-  //   const icon = await sut({ type: 'add', size });
-  //   expect(icon.getAttribute('width')).toBe(`${size}px`);
-  // });
+  it('should render icon with custom size', async () => {
+    const size = 60;
+    const icon = await sut({ type: 'add', size });
+    expect(icon.style.width).toBe(`${size}px`);
+  });
 
-  // it('should render icon with custom color', async () => {
-  //   const color = 'blue';
-  //   const icon = await sut({ type: 'add', color });
-  //   expect(icon.getAttribute('stroke')).toBe(color);
-  // });
+  it('should render icon with custom color', async () => {
+    const color = 'purple';
+    const icon = await sut({ type: 'add', color });
+    expect(icon.style.stroke).toBe(color);
+  });
 
-  it('should render fill transparent in some icons', async () => {
+  it('should render fill transparent in all icons', async () => {
     const color = 'transparent';
-    expect((await sut()).getAttribute('fill')).toBe(color);
+    expect((await sut({
+      type: 'arrow-down'
+    })).style.fill).toBe(color);
   });
 });
