a29d3b082b6eb22c32316baff8bf783fc4f04ff7
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-11-07 10:50:56 -0300

    :books: adding items to documentation :docs

--- src/app/components/button/button.stories.ts
+++ src/app/components/button/button.stories.ts
@@ -1,9 +1,24 @@
 import { ButtonComponent } from './button.component';
-
+import { Meta, Story } from '@storybook/angular';
 export default {
-    title: 'Components/Button',
+    title: 'Components/Buttons',
     component: ButtonComponent,
-}
+} as Meta;
 
+const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
+    component: ButtonComponent,
+    props: args,
+});
 
+export const Primary = Template.bind({});
+Primary.args = {
+    label:'Primary',
+    type: 'primary',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+    label:'Secondary',
+    type: 'secondary',
+};
 
