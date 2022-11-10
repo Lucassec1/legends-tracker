02fa8cf696ba6e6df75b4d8acc1cc64ece1ed819
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-11-10 10:10:40 -0300

    :bug: render champion in card :fix(#35)

--- src/app/components/card/card.stories.ts
+++ src/app/components/card/card.stories.ts
@@ -3,27 +3,33 @@ import { Meta, Story } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
 import { AppIconComponent } from '../../components/icon/icon.component';
 export default {
-    title: 'Lib/Card/card',
-    component: CardComponent,
+  title: 'Lib/Card/card',
+  component: CardComponent,
 } as Meta;
 
 const Template: Story<CardComponent> = (args: CardComponent) => ({
-    component: CardComponent,
-    props: args,
-    moduleMetadata: {
-        declarations: [CardComponent, AppIconComponent],
-        imports: [CommonModule],
-    },
+  component: CardComponent,
+  props: args,
+  moduleMetadata: {
+    declarations: [CardComponent, AppIconComponent],
+    imports: [CommonModule],
+  },
 });
 
 export const CardSimple = Template.bind({});
 CardSimple.args = {
-    victory: 'true',
-    firstSummonerSpell: "cleanse",
-    secondSummonerSpell: "teleport",
-    kill: 10,
-    //championName: 'ahri',
-    champion: 'lee sin',
-    death: 5,
-    assistance: 10,
+  type: "match-history",
+  victory: 'true',
+  firstSummonerSpell: 'cleanse',
+  secondSummonerSpell: 'heal',
+  kill: 10,
+  champion: "evelynn",
+  championName: "",
+  //champion: "LeeSin",
+  //championSquareImgURL: "https://i.pinimg.com/564x/59/74/d0/5974d04323d9efbaf170c72cfdb07b44.jpg",
+  /*championName() {
+      return this.champion = 'lee sin';
+  },*/
+  death: 5,
+  assistance: 10,
 };
