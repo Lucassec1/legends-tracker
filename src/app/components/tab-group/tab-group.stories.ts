6b2e58c73a5e2fd86aa7d5f37ecf8f637c67bbce
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-11-11 10:45:58 -0300

    :bug: render tab-group in the storybook :fix(#43)

--- src/app/components/tab-group/tab-group.stories.ts
+++ src/app/components/tab-group/tab-group.stories.ts
@@ -1,9 +1,10 @@
-import { TabGroup, TabGroupComponent } from './tab-group.component';
+import { TabGroupComponent } from './tab-group.component';
 import { Meta, Story } from '@storybook/angular';
 import { CommonModule } from '@angular/common';
-import { AppIconComponent } from '../../components/icon/icon.component';
+import { action } from '@storybook/addon-actions';
+import { TabComponent } from '../tab/tab.component'
 export default {
-    title: 'Lib/Tab-Group/Tab-Group',
+    title: 'Lib/Tab-Group/tab-Group',
     component: TabGroupComponent,
 } as Meta;
 
@@ -11,14 +12,152 @@ const Template: Story<TabGroupComponent> = (args: TabGroupComponent) => ({
     component: TabGroupComponent,
     props: args,
     moduleMetadata: {
-        declarations: [TabGroupComponent, AppIconComponent],
+        declarations: [TabGroupComponent, TabComponent],
         imports: [CommonModule],
     },
 });
 
-export const TypeAdc = Template.bind({});
-TypeAdc.args = {
-    border: "bottom",
-    size: 'md',
-    
+export const TypeHorizontal = Template.bind({});
+TypeHorizontal.args = {
+  tabs: [
+    {
+      label: 'Selected',
+      selected: true,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+  ],
+  border: "bottom",
+  direction: "horizontal",
 };
+
+export const TypeVertical = Template.bind({});
+TypeVertical.args = {
+  tabs: [
+    {
+      label: 'Selected',
+      selected: true,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+  ],
+  border: "bottom",
+  direction: "vertical",
+};
+
+export const TypeBorderLeft = Template.bind({});
+TypeBorderLeft.args = {
+  tabs: [
+    {
+      label: 'Selected',
+      selected: true,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+  ],
+  border: "left",
+  direction: "vertical",
+};
+
+export const TypeBorderBottom = Template.bind({});
+TypeBorderBottom.args = {
+  tabs: [
+    {
+      label: 'Selected',
+      selected: true,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+  ],
+  border: "bottom",
+  direction: "vertical",
+};
+
+export const TypeBorderRight = Template.bind({});
+TypeBorderRight.args = {
+  tabs: [
+    {
+      label: 'Selected',
+      selected: true,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+  ],
+  border: "right",
+  direction: "vertical",
+};
+
+export const TypeBorderTop = Template.bind({});
+TypeBorderTop.args = {
+  tabs: [
+    {
+      label: 'Selected',
+      selected: true,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+    {
+      label: 'Not Selected',
+      selected: false,
+    },
+  ],
+  border: "top",
+  direction: "vertical",
+};
+
