3be4259171d41820fa97b92c8b4ed078f6d6dc98
IaraFV <103749046+IaraFV@users.noreply.github.com>
2022-11-16 08:10:23 -0300

    :recycle: refactor the new storybook :refactor

---
+++ src/app/components/button/button.stories.ts
@@ -0,0 +1,75 @@
+import { ButtonComponent } from './button.component';
+import { Meta, Story } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+import { AppIconComponent } from '../icon/icon.component';
+
+export default {
+    title: 'Lib/Button/button',
+    component: ButtonComponent,
+} as Meta;
+
+const Template: Story<ButtonComponent> = (args: ButtonComponent) => ({
+    component: ButtonComponent,
+    props: args,
+    moduleMetadata: {
+        declarations: [ButtonComponent, AppIconComponent],
+        imports: [CommonModule],
+    },
+});
+
+export const TypePrimary = Template.bind({});
+TypePrimary.args = {
+    label:'Primary',
+    type: 'primary',
+};
+
+export const TypeSecondary = Template.bind({});
+TypeSecondary.args = {
+    label:'Secondary',
+    type: 'secondary',
+};
+
+export const TypeGhost = Template.bind({});
+TypeGhost.args = {
+    label: 'Ghost',
+    type: 'ghost',
+};
+
+export const TypeSm =  Template.bind({});
+TypeSm.args = {
+    label: 'sm',
+    size: "sm"
+}
+
+export const TypeMd =  Template.bind({});
+TypeMd.args = {
+    label: 'md',
+    size: "md",
+}
+
+export const TypeLg =  Template.bind({});
+TypeLg.args = {
+    label: 'lg',
+    size: "lg",
+}
+
+export const TypeAdd = Template.bind({});
+TypeAdd.args = {
+    iconType: "add",
+}
+
+export const TypePosition = Template.bind({});
+TypePosition.args = {
+    iconType: "position-bot",
+}
+
+export const TypeClose = Template.bind({});
+TypeClose.args = {
+    iconType: "close",
+    label: "Close",
+}
+
+export const TypeLoading = Template.bind({});
+TypeLoading.args = {
+    loading: true,
+}
---
+++ src/app/components/divider/divider.stories.ts
@@ -0,0 +1,26 @@
+import { DividerComponent } from './divider.component';
+import { Meta, Story } from '@storybook/angular';
+import { CommonModule } from '@angular/common';
+export default {
+    title: 'Lib/Divider/divider',
+    component: DividerComponent,
+} as Meta;
+
+const Template: Story<DividerComponent> = (args: DividerComponent) => ({
+    component: DividerComponent,
+    props: args,
+    moduleMetadata: {
+        declarations: [DividerComponent],
+        imports: [CommonModule],
+    },
+});
+
+export const TypeHorizontal = Template.bind({});
+TypeHorizontal.args = {
+    direction: "horizontal"
+};
+
+export const TypeVertical = Template.bind({});
+TypeVertical.args = {
+    direction: "vertical",
+};
